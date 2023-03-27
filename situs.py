f2664366b578e43fed0f693e55c6e69a655ef5e2
Eduardo Quin <94932412+EdyQuin@users.noreply.github.com>
2023-03-27 18:25:20 -0400

    Update situs.py

--- situs.py
+++ situs.py
@@ -1,10 +1,41 @@
 import streamlit as st
 import pandas as pd
 from io import StringIO
+from deta import Deta
 
 st.image('./LOGO_091622.png')
 st.header('SITUS')
-st.subheader('Property Management')
+st.subheader('Property Management Solution')
+
+# Data to be written to Deta Base
+with st.form("form"):
+    name = st.text_input("property name")
+    address = st.number_input("property address")
+    manager = st.text_input("manager name")
+    floors = st.number_input("how many floors")
+    qone = st.text_input("What")
+    qtwo = st.text_input("What ")
+    qthree = st.text_input("Are ")
+    
+    submitted = st.form_submit_button("Submit your property reqport")
+    clear_on_submit=True
+    
+# Connect to Deta Base with your Project Key
+deta = Deta(st.secrets["deta_key"])
+db = deta.Base("Notary_Registration_App")
+
+# If the user clicked the submit button
+# write the data from the form to the database.
+# You can store any data you want here. Just modify that dictionary below (the entries between the {}).
+if submitted:
+    db.put({"name": name, "age": age, "email": email, "telephone": telephone, 
+            "q1": qone, "q2": qtwo, "q3": qthree})
+    if submitted:
+        st.write("Your answers have been successfully received. For any questions or concerns please contact the office @ equin@assetmana.com. Please close your browser when you are finished.")
+"---"
+# This reads all items from the database and displays them to your app.
+# db_content is a list of dictionaries. You can do everything you want with it.
+db_content = db.fetch(query=None, limit=None, last=None).items
 
 # uploaded_file = st.file_uploader("Choose a file")
 # if uploader_file is not None:
@@ -59,21 +90,3 @@ photo_5 = st.camera_input("Take photo 5")
 if photo_5:
     st.image(photo_5)
 
-
-    
-# Connect to Deta Base with your Project Key
-deta = Deta(st.secrets["deta_key"])
-db = deta.Base("SITUS")
-
-# If the user clicked the submit button
-# write the data from the form to the database.
-# You can store any data you want here. Just modify the dictionary below (the entries between the {}).
-if submitted:
-    db.put({"name": name, "address": address, "date": date, "manager": manager, 
-            "q1": qone, "q2": qtwo, "q3": qthree})
-    if submitted:
-        st.write("Your reports has been successfully received. For any questions or concerns please contact the office @ equin@assetmana.com. Please close your browser when you are finished.")
-"---"
-# This reads all items from the database and displays them to your app.
-# db_content is a list of dictionaries. You can do everything you want with it.
-db_content = db.fetch(query=None, limit=None, last=None).items
