e2827b2527fd281dc59fc830f7b98e2706bdd5da
Eduardo Quin <94932412+EdyQuin@users.noreply.github.com>
2023-03-27 18:07:53 -0400

    Update situs.py

--- situs.py
+++ situs.py
@@ -11,13 +11,14 @@ st.subheader('Real Tech Management')
     # To read file as bytes:
    # byte_data = uploaded_file.getvalue()
     # st.write(bytes_data)
-    
-    uploaded_files = st.file_uploader("Choose a CSv file", accept_multiple_files=True)
-    for uploaded_file in uploaded_files:
-        bytes_data = uploaded_file.read()
-        st.write("filename:", uploaded_file.name)
-        st.write(bytes_data)
-    # To convert to a stringbased IO:
+
+uploaded_files = st.file_uploader("Choose a CSv file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
+
+# To convert to a stringbased IO:
     strongio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     
     st.write(stringio)
